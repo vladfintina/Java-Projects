4995a527b65ac5db2e101647d7a36a805147c48b
Vlad Fintina <vladfintina@gmail.com>
2023-03-08 00:25:46 +0200

    QuickSort

---
+++ QuickSort/QuickSort_HW5/src/Main.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+
+    static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static int partition(int[] arr, int low, int high)
+    {
+
+        // pivot
+        int pivot = arr[high];
+
+        // Index of smaller element and
+        // indicates the right position
+        // of pivot found so far
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+
+            // If current element is smaller
+            // than the pivot
+            if (arr[j] < pivot) {
+
+                // Increment index of
+                // smaller element
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    /* The main function that implements QuickSort
+              arr[] --> Array to be sorted,
+              low --> Starting index,
+              high --> Ending index
+     */
+    static void quickSort(int[] arr, int low, int high)
+    {
+        if (low < high) {
+
+            // pi is partitioning index, arr[p]
+            // is now at right place
+            int pi = partition(arr, low, high);
+
+            // Separately sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = new int[args.length];
+        try {
+            int i = 0;
+            for(i = 0; i< args.length; i++)
+            {
+                int number = Integer.parseInt(args[i]);
+                myArray[i] = number;
+
+
+            }
+
+            Main.quickSort(myArray,0, i-1 );
+            for(int j=0;j < i;j++)
+                System.out.println(myArray[j]);
+        }
+        catch(Exception ex)
+        {
+            //System.out.println(ex);
+            System.out.println("Input error");
+        }
+
+
+    }
+}
